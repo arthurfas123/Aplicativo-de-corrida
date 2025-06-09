5d544b2791bc2e65ac7d3dc306b6f58e00db8767
Arthur <Arthurfas123@gmail.com>
2025-06-09 15:57:49 -0300

    arrumei o bug do arquivo zerar

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,7 @@ import 'controllers/UserController.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (UserController.instance.usuarios.isEmpty) {
-    await UserController.instance.cadastrarUsuario('teste', '123');
-    print('Usuário teste criado');
-  }
+
   await UserController.instance.carregarUsuarios();
   runApp(AppWidget());
 }
